f24b9a17649311819f63fb5822a0a55f9f6410e9
lauramanuputty <lauramanuputty@users.noreply.github.com>
2020-03-16 23:54:52 +0100

    Html & CSS challenge 2
    Dag 1) Html & CSS Mobile first

---
+++ geboortedag.js
@@ -0,0 +1,7 @@
+var today = new Date();
+
+var birthdayLaura = new Date(2000, 6, 30); //30 juli 2000
+
+document.getElementById(‘clock’)innerHTML = birthdayLaura.getTime();
+
+console.log(birthdayLaura).getTime());
