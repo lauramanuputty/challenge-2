b6a08041590c4a48f86838e0f7aa0102ff175d62
lauramanuputty <55565669+lauramanuputty@users.noreply.github.com>
2020-03-26 22:52:02 +0100

    Update klok.js
    De layout aangepast

--- klok.js
+++ klok.js
@@ -1,5 +1,5 @@
 
-	function startTijd(){
+function startTijd(){
     var vandaag = new Date();
     var uur = vandaag.getHours();
     var min = vandaag.getMinutes();
@@ -9,15 +9,15 @@
     sec = checkTijd(sec);
     document.getElementById("klok").innerHTML = uur + " : " + min + " : " + sec;
     var tijd = setTimeout(function(){ startTijd() }, 500);
-	}
+}
 
-	function checkTijd(i){
-		if (i < 10) {
-			i = "0" + i;
-		}
-		return i;
+function checkTijd(i){
+	if (i < 10) {
+		i = "0" + i;
 	}
+	return i;
+}
 
-	window.onload = function(){
-		startTijd();
-	}
+window.onload = function(){
+	startTijd();
+}
