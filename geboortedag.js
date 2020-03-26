858ed956662fd7cf9e2805ae916a4382392b5ba2
lauramanuputty <55565669+lauramanuputty@users.noreply.github.com>
2020-03-26 22:14:55 +0100

    Delete geboortedag.js
    Dit is een opdracht van tijdens de les.

--- geboortedag.js
+++
@@ -1,7 +0,0 @@
-var today = new Date();
-
-var birthdayLaura = new Date(2000, 6, 30); //30 juli 2000
-
-document.getElementById(‘clock’)innerHTML = birthdayLaura.getTime();
-
-console.log(birthdayLaura).getTime());
