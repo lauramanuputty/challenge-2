7d1b7a1b996f12722f6e10381e9fde6f14e72472
lauramanuputty <lauramanuputty@users.noreply.github.com>
2020-03-27 09:36:35 +0100

    Achtergrond kleur verandert in javascript bestand

--- klok.js
+++ klok.js
@@ -1,5 +1,5 @@
 
-	function startTijd(){
+function startTijd(){
     var vandaag = new Date();
     var uur = vandaag.getHours();
     var min = vandaag.getMinutes();
@@ -8,16 +8,28 @@
     min = checkTijd(min);
     sec = checkTijd(sec);
     document.getElementById("klok").innerHTML = uur + " : " + min + " : " + sec;
-    var tijd = setTimeout(function(){ startTijd() }, 500);
+    var tijd = setTimeout(function(){ startTijd() }, 500);	
+		
+		if(uur >= 6 && uur < 13){
+			document.body.style.backgroundColor = "#F5B070";
+		}
+			
+		else if (uur >= 18 && uur < 24){
+			document.body.style.backgroundColor = "#80B0E8";
+		}
+				
+		else{
+			document.body.style.backgroundColor= "#09203A";
+		}
 	}
-
+	
 	function checkTijd(i){
 		if (i < 10) {
 			i = "0" + i;
 		}
 		return i;
 	}
-
+	
 	window.onload = function(){
 		startTijd();
 	}
