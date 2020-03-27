82c1e097224efe662a8c1922b3d155b8d108826a
lauramanuputty <lauramanuputty@users.noreply.github.com>
2020-03-27 11:53:57 +0100

    Kleurenverandering
    Kleuren doen het niet op de website

--- klok.js
+++ klok.js
@@ -11,7 +11,7 @@ function startTijd(){
     var tijd = setTimeout(function(){ startTijd() }, 500);	
 		
 		if(uur >= 6 && uur < 12){
-			document.body.style.backgroundColor = "orange";
+			document.body.style.backgroundColor = "#c2d7f0";
 		}
 			
 		else if (uur >= 12 && uur < 18){
@@ -36,5 +36,5 @@ function checkTijd(i){
 
 window.onload = function(){
 	startTijd();
-}
+};
 
