291e815aadc5d76efbe0a2bbffae3a2283fb43da
lauramanuputty <lauramanuputty@users.noreply.github.com>
2020-03-27 12:00:28 +0100

    structuur  verandert

--- klok.js
+++ klok.js
@@ -10,21 +10,21 @@ function startTijd(){
     document.getElementById("klok").innerHTML = uur + " : " + min + " : " + sec;
     var tijd = setTimeout(function(){ startTijd() }, 500);	
 		
-		if(uur >= 6 && uur < 12){
-			document.body.style.backgroundColor = "#c2d7f0";
-		}
+	if(uur >= 6 && uur < 13){
+		document.body.style.backgroundColor = "#c2d7f0";
+	}
 			
-		else if (uur >= 12 && uur < 18){
-			document.body.style.backgroundColor = "#80B0E8";
-		}
+	else if(uur >= 13 && uur < 18){
+		document.body.style.backgroundColor = "#80B0E8";
+	}
 		
-		else if (uur >= 18 && uur < 24 ){
-			document.body.style.backgroundColor = "#5D82AE";
-		}
+	else if (uur >= 18 && uur < 24 ){
+		document.body.style.backgroundColor = "#5D82AE";
+	}
 				
-		else{
-			document.body.style.backgroundColor = "#09203A";
-		}
+	else{
+		document.body.style.backgroundColor = "#09203A";
+	}
 };
 
 function checkTijd(i){
